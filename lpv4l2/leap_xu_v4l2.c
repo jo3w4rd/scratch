4601cfe8560499a99399a7a0425a29cb868cecab
Joe Ward <jward@leapmotion.com>
2016-09-24 12:19:26 -0700

    pointer syntax fiddling

--- lpv4l2/leap_xu_v4l2.c
+++ lpv4l2/leap_xu_v4l2.c
@@ -19,8 +19,8 @@ long leap_xu_ioctl_default(struct file *file, void *fh, bool valid_prio, unsigne
              struct uvc_xu_control_query xu_query = {};
              __u16 length;
              get_user(arg->size, length);
-             if(copy_from_user(xu_query, arg, sizeof(struct uvc_xu_control_query))){
-                 return handle_xu_operation(fh, valid_prio, xu_query);
+             if(copy_from_user(&xu_query, arg, sizeof(struct uvc_xu_control_query))){
+                 return handle_xu_operation(fh, valid_prio, &xu_query);
              } else {
                  return -EFAULT;
              }
