5690920f2215236d8f37f03859efd191991a04d7
Joe Ward <jward@leapmotion.com>
2016-09-25 15:08:30 -0700

    implementing len info

--- lpv4l2/leap_xu_v4l2.c
+++ lpv4l2/leap_xu_v4l2.c
@@ -24,14 +24,15 @@ struct leap_xu_ctrl{
 };
 
 
-static const struct leap_xu_ctrl leap_xu_ctrls[] = {};
+static const struct leap_xu_ctrl leap_xu_ctrls[] = {
         {LEAP_XU_STROBE_WIDTH, sizeof(u32_t), NULL, NULL},
         {LEAP_XU_LED_POSITIONS, sizeof(u8_t), NULL, NULL}
     };
 static const int num_leap_xu_ctrls = sizeof( leap_xu_ctrls ) / sizeof( leap_xu_ctrls[0] );
 
 static int find_leap_xu_ctrl(__u8 selector, struct leap_xu_ctrl *ctrl){
-    for(int c = 0; c < num_leap_xu_ctrls; c++){
+    int c =0;
+    for(; c < num_leap_xu_ctrls; c++){
         if(leap_xu_ctrls[c].selector == selector){
             ctrl = leap_xu_ctrls[c];
             return 0;
